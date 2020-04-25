6132fda08171e062013c85f44822a43fca01d4e1
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2020-04-25 18:05:22 +0530

    Initial commit

---
+++ demoApp/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapScreen from './MapScreen'
+
+/*
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+*/
+export default () => (
+  <MapScreen />
+);
---
+++ demoApp/index.js
@@ -0,0 +1,6 @@
+
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import MapScreen from './MapScreen'
+
+AppRegistry.registerComponent('webviewdemo', () => MapScreen);
